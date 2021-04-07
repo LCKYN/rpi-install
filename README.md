a6f2074dcab2cc9e8edf34b0ae4a42d51827d6b0
Pawarit Thareechan <root@lckyn.com>
2021-04-07 16:17:53 +0700

    add install docker

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rpi-install
+## DOCKER
---
+++ install-docker.sh
@@ -0,0 +1,18 @@
+curl -sSL https://get.docker.com | sh
+
+sudo usermod -aG docker ubuntu
+
+# test
+docker run hello-world
+
+
+sudo apt-get install -y libffi-dev libssl-dev
+
+sudo apt-get install -y python3 python3-pip
+
+sudo apt-get remove python-configparser
+
+
+sudo pip3 -v install docker-compose
+
+# done 
